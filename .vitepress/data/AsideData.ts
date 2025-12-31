d8365d3e8935e1d8ca7b563f23a1ca031d205725
Theo <fanxiaobin422@gmail.com>
2025-12-31 11:37:56 +0800

    chore: update promo

--- .vitepress/data/AsideData.ts
+++ .vitepress/data/AsideData.ts
@@ -175,7 +175,7 @@ export const Aside_Data: AsideItem = [
   //   rel: 'sponsored noreferrer'
   // },
   {
-    image: 'https://i.theojs.cn/logo/hero-sms.webp',
+    image: 'https://i.theojs.cn/logo/hero-sms.svg',
     promo: 'Hero-SMS 短信接码',
     info1: '秒开 <span class="promo-text">虚拟号码</span> 注册无障碍',
     info2: '覆盖 <span class="promo-text">200+国家</span> 轻松可用',
